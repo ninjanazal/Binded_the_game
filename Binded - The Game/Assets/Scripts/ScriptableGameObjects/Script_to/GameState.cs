9aec80f4106b962f033a28172422d0f38b468e68
Eurico Martins <euricomartinss@gmail.com>
2020-03-29 18:30:13 +0100

    Fixed terrain And grass on gaol, working on gameManagerSystem

---
+++ Binded - The Game/Assets/Scripts/ScriptableGameObjects/Script_to/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// enumerdo dos niveis existentes
+public enum LevelName
+{ Gaol, Hamr, HamrBeacon, Hugr, HugrBeacon, Fylgja, FulgjaBeacon, Hamingja, HamingjaBeacon }
+
+
+[CreateAssetMenu(fileName = "GameState", menuName = "Binded/GameState")]
+public class GameState : ScriptableObject
+{
+    // variaveis de nivel
+    // indicaçao em que nivel o jogador esta
+    [SerializeField] private LevelName _CurrentLevel = LevelName.Gaol;
+    // indica qual o proximo nivel 
+
+
+
+
+
+    // getter publico para obter o nivel actual
+    public LevelName CurrentLevel { get { return _CurrentLevel; } }
+}
